41b616388b4739fcea03cb8566d27482337b6053
VicRH24 <69468016+victor-rivera24@users.noreply.github.com>
2023-11-09 12:53:20 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ data "aws_availability_zones" "available" {
 }
 
 locals {
-  cluster_name = "education-eks-${random_string.suffix.result}"
+  cluster_name = var.cluster_name
 }
 
 resource "random_string" "suffix" {

505b5f081a6d9f0a67b8563739536b36ac5c636b
VicRH24 <69468016+victor-rivera24@users.noreply.github.com>
2023-11-09 12:52:04 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,3 +6,23 @@ variable "region" {
   type        = string
   default     = "us-east-2"
 }
+
+variable "VPC_ID" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "Subnets" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "AccessKey" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "SecretKey" {
+  description = "AWS region"
+  type        = string
+}
